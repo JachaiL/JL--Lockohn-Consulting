e490fb25b3f4d44c3ed1f855c59e8f171d13ce9b
JachaiL <jachai.lockhart@gmail.com>
2026-03-11 11:32:27 -0500

    Update Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Us</h2>
+      <p>Get in touch with Lockohn Consulting to discuss your project needs.</p>
+      <ul>
+        <li>Email: info@lockohnconsulting.com</li>
+        <li>Location: United States</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Contact
