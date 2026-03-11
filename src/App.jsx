ddf73a1be0f0d0414b8d4c9731303e1d22e21e09
JachaiL <jachai.lockhart@gmail.com>
2026-03-11 12:33:46 -0500

    Add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import './App.css'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Services from './pages/Services.jsx'
+import FoodProcessing from './pages/FoodProcessing.jsx'
+import Manufacturing from './pages/Manufacturing.jsx'
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/food-processing" element={<FoodProcessing />} />
+        <Route path="/manufacturing" element={<Manufacturing />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
