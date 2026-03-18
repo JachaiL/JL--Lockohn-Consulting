da9e3607c13efced880e395f5479e9eab73b1143
JachaiL <jachai.lockhart@gmail.com>
2026-03-18 16:45:05 -0500

    Update Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Services from './Services'
+import About from './About'
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Services />
+      <About />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
